ebb13eec3f777708e011422b1eba4c046f2308c2
javabritto <brittoinn5@gmail.com>
2020-02-29 12:19:14 +0530

    added number reverse changes

--- src/org/overloading/sample/Staticm.java
+++ src/org/overloading/sample/Staticm.java
@@ -1,11 +1,30 @@
 package org.overloading.sample;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public  class Staticm {
 	
  public Staticm(int a) {
 	// TODO Auto-generated constructor stub
 System.out.println("my parent parametrized constructor");
- }
+int b =0, c=58253, d=0,temp =0;
+List<Integer> li =new ArrayList<Integer>();
+
+while(c>0)
+{
+	b = c%10;
+	c =c/10;
+	li.add(b);
 	
+}
+Collections.sort(li); 
+for (int i = 0; i < li.size(); i++) {
+	 System.out.print(li.get(i));
+	
+}
+ }
+ 
 
 }
