36f46263da5bb523642d9ec00a48317a8f29af86
dhakshuA <dhakshua@gmail.com>
2020-03-02 01:10:40 +0530

    moving

---
+++ src/org/overloading/OverLoad2.java
@@ -0,0 +1,12 @@
+package org.overloading;
+
+public class OverLoad2  {
+
+	public static void overmeth() {
+	System.out.println(" This is OverLoad2 of default meth");
+	}
+	public static void main(String[] args) {
+		overmeth();
+		
+	}
+}
